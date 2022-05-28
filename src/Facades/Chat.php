257283da658a1881b90c5829b23f730f23cb2de3
Salvation Arinze <modernbabbage@gmail.com>
2022-05-28 15:49:03 +0100

    fix: Make chat facade better

--- src/Facades/Chat.php
+++ src/Facades/Chat.php
@@ -4,7 +4,25 @@ namespace Musonza\Chat\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use Musonza\Chat\Chat as BaseChat;
+use Musonza\Chat\Models\Conversation;
+use Musonza\Chat\Models\MessageNotification;
+use Musonza\Chat\Services\ConversationService;
+use Musonza\Chat\Services\MessageService;
 
+/**
+ * @method static Conversation createConversation(array $participants, array $data = [])
+ * @method static Conversation makeDirect()
+ * @method static MessageService message($message)
+ * @method static MessageService messages()
+ * @method static ConversationService conversation(Conversation $conversation)
+ * @method static ConversationService conversations()
+ * @method static MessageNotification unReadNotifications()
+ * @method static bool broadcasts()
+ * @method static bool sentMessageEvent()
+ * @method static array|null senderFieldsWhitelist()
+ *
+ * @see \Musonza\Chat\Chat
+ */
 class Chat extends Facade
 {
     /**
